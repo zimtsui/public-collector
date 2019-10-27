5f7256a59cab85111617e969c60ca8d2f7b48502
Zim <zimtsui@gmail.com>
2019-10-27 09:51:13 +0800

    dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,6 +13,6 @@ module.exports = {
             STOP_TIMEOUT: 20000,
             EXIT_TIMEOUT: 5000,
         },
-        args: './data/2019-10-26.db',
+        args: './data/2019-10-27.db',
     }],
 };
